31736d05e016ffe2b7b7f8dda691698207f3a467
NotTheDr01ds <32344964+NotTheDr01ds@users.noreply.github.com>
2024-04-05 06:24:21 -0400

    Use current site and user. Expanded to other SE sites.

--- GetSOFlags.user.js
+++ GetSOFlags.user.js
@@ -1,17 +1,27 @@
 // ==UserScript==
-// @name         Get SO Flags
+// @name         Get SE Post Flags
 // @namespace    http://tampermonkey.net/
 // @version      2024-04-03
-// @description  Retrieve 
+// @description  Retrieve flags for a Stack Exchange site as JSON to clipboard
 // @author       NotTheDr01ds
-// @match        https://stackoverflow.com/users/flag-summary/11810933*
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=stackoverflow.com
+// @match        https://stackoverflow.com/users/flag-summary/*
+// @match        https://askubuntu.com/users/flag-summary/*
+// @match        https://superuser.com/users/flag-summary/*
+// @match        https://serverfault.com/users/flag-summary/*
+// @match        https://mathoverflow.net/users/flag-summary/*
+// @match        https://stackapps.com/users/flag-summary/*
+// @match        https://*.stackexchange.com/users/flag-summary/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=stackexchange.com
 // @grant        GM_registerMenuCommand
 // @grant        GM_setClipboard
 // ==/UserScript==
 
 const msBetweenRequests = 2000;
-const firstPage = "https://stackoverflow.com/users/flag-summary/11810933?group=1&page=1";
+
+const userIdRegex = /\/users\/flag-summary\/(\d+)/
+const userId = window.location.pathname.match(userIdRegex)[1];
+const firstPage = `${window.location.origin}/users/flag-summary/${userId}?group=1&page=1`;
+const domain = window.location.origin;
 
 async function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve,ms));
@@ -24,17 +34,18 @@ async function getHtmlDoc(url) {
 	return parser.parseFromString(html, "text/html");
 }
 
-async function menu (event) {
+async function userScriptGetFlagsMenu(event) {
+
   let doc = await getHtmlDoc(firstPage);
   const NUM_PAGES = parseInt([...doc.querySelectorAll(".s-pagination--item")].slice(-2,-1)[0].textContent)
   console.log(`Retrieving ${NUM_PAGES} total pages`)
   
   let flags = [];
-  for (let pageNum = 1; pageNum <= NUM_PAGES; pageNum++) {
-  //for (let pageNum = 1; pageNum <= 2; pageNum++) {
+  //for (let pageNum = 1; pageNum <= NUM_PAGES; pageNum++) {
+  for (let pageNum = 1; pageNum <= 2; pageNum++) {
     sleep(msBetweenRequests);
     console.log(`Retrieving page ${pageNum}`);
-    let url = `https://stackoverflow.com/users/flag-summary/11810933?group=1&page=${pageNum}`;
+    let url = `${domain}/users/flag-summary/${userId}?group=1&page=${pageNum}`;
     let doc = await getHtmlDoc(url);
     flags.push(
       ...(
@@ -48,11 +59,11 @@ async function menu (event) {
         if (post.querySelector('.answer-hyperlink')) {
           postType = "answer"
           postId = parseInt(post.querySelector(".answer-hyperlink").href.split("#")[1]);
-          postLink = `https://stackoverflow.com/a/${postId}`
+          postLink = `${domain}/a/${postId}`
         } else if (post.querySelector('.question-hyperlink')) {
           postType = "question"
           postId = parseInt(post.querySelector('.question-hyperlink').href.split('/')[4]);
-          postLink = `https://stackoverflow.com/q/${postId}`
+          postLink = `${domain}/q/${postId}`
         }
 
         let flagText = flag.querySelector('.revision-comment')?.textContent.trim();
@@ -67,13 +78,13 @@ async function menu (event) {
 
         return {
           postId: postId,
-          link: postLink,
+          postLink: postLink,
           postTitle: postTitle,
           postType: postType,
           flagText: flagText,
           flagHtml: flagHtml,
           flagCreatedTime: flagCreatedTime,
-          status: status,
+          flagStatus: status,
           modMessage: modMessage,
         }
       })
@@ -88,5 +99,5 @@ async function menu (event) {
 (function() {
   'use strict';
 
-  const menu_command_id_1 = GM_registerMenuCommand("Get Flags", menu);
+  const menu_command_id_1 = GM_registerMenuCommand("Get Post Flags", userScriptGetFlagsMenu);
 })();

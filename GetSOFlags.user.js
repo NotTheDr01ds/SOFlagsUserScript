14b750be6ba2bd16d9d664dfd2fc6396636487d0
NotTheDr01ds <32344964+NotTheDr01ds@users.noreply.github.com>
2024-04-04 21:07:51 -0400

    Initial commit

---
+++ GetSOFlags.user.js
@@ -0,0 +1,92 @@
+// ==UserScript==
+// @name         Get SO Flags
+// @namespace    http://tampermonkey.net/
+// @version      2024-04-03
+// @description  Retrieve 
+// @author       NotTheDr01ds
+// @match        https://stackoverflow.com/users/flag-summary/11810933*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=stackoverflow.com
+// @grant        GM_registerMenuCommand
+// @grant        GM_setClipboard
+// ==/UserScript==
+
+const msBetweenRequests = 2000;
+const firstPage = "https://stackoverflow.com/users/flag-summary/11810933?group=1&page=1";
+
+async function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve,ms));
+}
+
+async function getHtmlDoc(url) {
+  await sleep(msBetweenRequests);
+	const html = await (await fetch(url)).text();
+	const parser = new DOMParser();
+	return parser.parseFromString(html, "text/html");
+}
+
+async function menu (event) {
+  let doc = await getHtmlDoc(firstPage);
+  const NUM_PAGES = parseInt([...doc.querySelectorAll(".s-pagination--item")].slice(-2,-1)[0].textContent)
+  console.log(`Retrieving ${NUM_PAGES} total pages`)
+  
+  let flags = [];
+  for (let pageNum = 1; pageNum <= NUM_PAGES; pageNum++) {
+  //for (let pageNum = 1; pageNum <= 2; pageNum++) {
+    sleep(msBetweenRequests);
+    console.log(`Retrieving page ${pageNum}`);
+    let url = `https://stackoverflow.com/users/flag-summary/11810933?group=1&page=${pageNum}`;
+    let doc = await getHtmlDoc(url);
+    flags.push(
+      ...(
+        [...doc.querySelectorAll(".mod-flag")]
+      ).map(flag => {
+        let post = flag.parentElement;
+        let postType;
+        let postId;
+        let postLink;
+
+        if (post.querySelector('.answer-hyperlink')) {
+          postType = "answer"
+          postId = parseInt(post.querySelector(".answer-hyperlink").href.split("#")[1]);
+          postLink = `https://stackoverflow.com/a/${postId}`
+        } else if (post.querySelector('.question-hyperlink')) {
+          postType = "question"
+          postId = parseInt(post.querySelector('.question-hyperlink').href.split('/')[4]);
+          postLink = `https://stackoverflow.com/q/${postId}`
+        }
+
+        let flagText = flag.querySelector('.revision-comment')?.textContent.trim();
+        let flagHtml = flag.querySelector('.revision-comment')?.innerHTML.trim(); 
+        let outcome = flag.querySelector('.flag-outcome')?.textContent || flag.querySelector('.mod-flag-indicator')?.textContent;
+        let outcomeParts = outcome.trim().split(" - ");
+        let status = outcomeParts[0].trim();
+        let modMessage = outcomeParts.slice(1).join(" - ");
+
+        let postTitle = (post.querySelector(".answer-hyperlink")||post.querySelector(".question-hyperlink"))?.textContent.trim();
+        let flagCreatedTime = flag.querySelector(".relativetime").getAttribute('title');
+
+        return {
+          postId: postId,
+          link: postLink,
+          postTitle: postTitle,
+          postType: postType,
+          flagText: flagText,
+          flagHtml: flagHtml,
+          flagCreatedTime: flagCreatedTime,
+          status: status,
+          modMessage: modMessage,
+        }
+      })
+    )
+  }
+
+  let jFlags = JSON.stringify(flags, null, " ");
+  GM_setClipboard(jFlags, "text", () => console.log("Flags have been placed on the clipboard as JSON."))
+  
+}
+
+(function() {
+  'use strict';
+
+  const menu_command_id_1 = GM_registerMenuCommand("Get Flags", menu);
+})();
